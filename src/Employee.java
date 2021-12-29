b62a3d9d0ff319a4a036e49903a875d362b7cde8
mukhlif <asaadmohamad39@gmail.com>
2021-12-29 23:08:24 +0200

    started gymProject

---
+++ src/Employee.java
@@ -0,0 +1,9 @@
+public class Employee extends StackeHolder {
+    protected String userName;
+    protected String passWord;
+    public Employee(String un, String pw, String f,String l,String g,int ph) {
+        super(f,l,g,ph);
+        this.userName=un;
+        this.passWord=pw;
+    }
+}
---
+++ src/StackeHolder.java
@@ -0,0 +1,15 @@
+public class StackeHolder {
+    protected String firstName;
+    protected String lastName;
+    protected String gender;
+    protected int phone;
+
+    protected StackeHolder(){}
+    protected StackeHolder(String f,String l,String g,int ph){
+        this.firstName=f;
+        this.lastName=l;
+        this.gender=g;
+        this.phone=ph;
+    }
+
+}
